5df5618f2e30f8f8b67ede92e661a68fb00cea1c
gabrielagv20 <gabriela.gomez.vega@ibm.com>
2025-02-07 04:28:11 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,23 @@
-// Created web server
+// Create web server
 var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Create HTTP server
+
+http.listen(port, function() {
+  console.log('listening on *:' + port);
+});
