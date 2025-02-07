9d5bdd95c54b629149fc7e029f14d2e7489c8f42
gabrielagv20 <gabriela.gomez.vega@ibm.com>
2025-02-07 04:14:40 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,42 @@
 // Created web server
-var http = require('http'),
-    url = require('url'),
-    fs = require('fs'),
-    path = require('path'),
-    util = require('util'),
-    crypto = require('crypto'),
-    querystring = require('querystring'),
-    marked = require('marked');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
 
-    
+var config = {
+    port: 8080,
+    root: '.',
+    index: 'index.html'
+};
+
+var server = http.createServer(function (req, res) {
+    var uri = url.parse(req.url).pathname;
+    var filename = path.join(config.root, uri);
+
+    fs.exists(filename, function (exists) {
+        if (!exists) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.write('404 Not Found\n');
+            res.end();
+            return;
+        }
+
+        if (fs.statSync(filename).isDirectory()) filename += '/' + config.index;
+
+        fs.readFile(filename, "binary", function (err, file) {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.write(err + '\n');
+                res.end();
+                return;
+            }
+
+            res.writeHead(200);
+            res.write(file, "binary");
+            res.end();
+        });
+    });
+});
+
+server.listen(config.port);
